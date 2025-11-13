b54f6fa1084d1b5c9393aedf5b90c2d3e2b8aa47
rauatorres <rauatorres@gmail.com>
2025-11-13 11:33:54 -0300

    finalizando tela de clientes - botao adicionar cliente

--- src/components/client_add_button/ClientAddButton.tsx
+++ src/components/client_add_button/ClientAddButton.tsx
@@ -2,9 +2,11 @@ import { MdAdd } from "react-icons/md";
 import type { Client } from "../../types/client";
 import { addClientToList } from "./script";
 
+import styles from "./clientAddButton.module.css";
+
 const ClientAddButton = (props: { clientList: Client[], setter: React.Dispatch<React.SetStateAction<Client[]>> }) => {
     return (
-        <button onClick={() => addClientToList(props.clientList, props.setter)} >novo cliente <MdAdd /></button>
+        <button className={styles.button} onClick={() => addClientToList(props.clientList, props.setter)} ><span>novo cliente</span><span className={styles.icon}><MdAdd /></span></button>
     )
 };
 
